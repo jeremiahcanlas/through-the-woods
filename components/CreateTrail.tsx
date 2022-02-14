bddec007bd0bf5187326d334188e83e164894b91
jeremiahcanlas <jeremiahcanlas@gmail.com>
2022-02-13 21:33:54 -0500

    minor fix

--- components/CreateTrail.tsx
+++ components/CreateTrail.tsx
@@ -27,141 +27,143 @@ const CreateTrail = () => {
     }, 3000);
   };
 
+  if (status === "loading") {
+    return <p>Please Wait</p>;
+  }
+
   if (status === "unauthenticated") {
     router.push("/");
     return <p>NOT AUTHORIZED</p>;
   }
 
-  if (status === "authenticated") {
-    const handleCreate = async (values) => {
-      const { title, location, description } = values;
+  const handleCreate = async (values) => {
+    const { title, location, description } = values;
 
-      try {
-        let files = [];
+    try {
+      let files = [];
 
-        //will upload to strapi backend
-        if (images.length >= 1) {
-          const data = new FormData();
+      //will upload to strapi backend
+      if (images.length >= 1) {
+        const data = new FormData();
 
-          images.forEach(async (img) => {
-            return data.append("files", img.originFileObj);
-          });
-
-          // uploads images to strapi media library
-          const res = await axios.post(`${server}/upload`, data, {
-            headers: {
-              Authorization: `Bearer ${session.jwt}`,
-            },
-          });
-
-          // this sets the image id in an array
-          await res.data.forEach((file) => {
-            files.push(file.id);
-          });
-        }
-
-        const json = JSON.stringify({
-          title: title,
-          location: location,
-          description: description,
-          images: files,
-          jwt: session.jwt,
+        images.forEach(async (img) => {
+          return data.append("files", img.originFileObj);
         });
 
-        const response = await axios.post("/api/trail/create", json, {
+        // uploads images to strapi media library
+        const res = await axios.post(`${server}/upload`, data, {
           headers: {
-            "Content-Type": "application/json",
+            Authorization: `Bearer ${session.jwt}`,
           },
         });
 
-        dispatch(
-          setAlert({
-            msg: "Successfully posted!",
-            alertType: "success",
-          })
-        );
-
-        router.push(`/trails/${response.data.id}`);
-      } catch (error) {
-        dispatch(
-          setAlert({
-            msg: "Post error, try again.",
-            alertType: "error",
-          })
-        );
+        // this sets the image id in an array
+        await res.data.forEach((file) => {
+          files.push(file.id);
+        });
       }
 
-      clearAlert();
-    };
+      const json = JSON.stringify({
+        title: title,
+        location: location,
+        description: description,
+        images: files,
+        jwt: session.jwt,
+      });
 
-    const validateForm = Yup.object({
-      title: Yup.string().required("Title is Required"),
-      location: Yup.string().required("Location is Required"),
-      description: Yup.string().required("Description is Required"),
-    });
+      const response = await axios.post("/api/trail/create", json, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-    return (
-      <PageContainer showImg={false}>
-        <Container my="2em" textAlign={["start", "start", "center"]}>
-          <Heading as="h2" fontSize="2rem" letterSpacing="2px">
-            Create
-          </Heading>
-        </Container>
-        <Container p={["0,2em", "0"]}>
-          <Formik
-            initialValues={{
-              title: "",
-              location: "",
-              description: "",
-            }}
-            validationSchema={validateForm}
-            onSubmit={handleCreate}
-          >
-            {({ isSubmitting, handleSubmit, handleReset, values }) => (
-              <Form onSubmit={handleSubmit}>
-                <TextField placeholder="Title" name="title" />
-                <TextField placeholder="Location" name="location" />
-                <TextField
-                  name="description"
-                  textbox={true}
-                  placeholder="Description"
-                />
-                <Box my="2em">
-                  <Heading fontSize={"1em"} mb="1em">
-                    Upload Images
-                  </Heading>
-                  <UploadFile images={images} setImages={setImages} />
-                </Box>
+      dispatch(
+        setAlert({
+          msg: "Successfully posted!",
+          alertType: "success",
+        })
+      );
 
-                <Stack direction="row" justifyContent="space-between">
-                  <Button
-                    isLoading={isSubmitting}
-                    disabled={isSubmitting}
-                    loadingText="Submitting"
-                    // width={["95%", "30%"]}
-                    size="lg"
-                    type="submit"
-                  >
-                    Submit
-                  </Button>
-                  <Button
-                    size="sm"
-                    colorScheme="red"
-                    onClick={handleReset}
-                    disabled={isSubmitting}
-                  >
-                    CLEAR
-                  </Button>
-                </Stack>
+      router.push(`/trails/${response.data.id}`);
+    } catch (error) {
+      dispatch(
+        setAlert({
+          msg: "Post error, try again.",
+          alertType: "error",
+        })
+      );
+    }
 
-                {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
-              </Form>
-            )}
-          </Formik>
-        </Container>
-      </PageContainer>
-    );
-  }
+    clearAlert();
+  };
+
+  const validateForm = Yup.object({
+    title: Yup.string().required("Title is Required"),
+    location: Yup.string().required("Location is Required"),
+    description: Yup.string().required("Description is Required"),
+  });
+
+  return (
+    <PageContainer showImg={false}>
+      <Container my="2em" textAlign={["start", "start", "center"]}>
+        <Heading as="h2" fontSize="2rem" letterSpacing="2px">
+          Create
+        </Heading>
+      </Container>
+      <Container p={["0,2em", "0"]}>
+        <Formik
+          initialValues={{
+            title: "",
+            location: "",
+            description: "",
+          }}
+          validationSchema={validateForm}
+          onSubmit={handleCreate}
+        >
+          {({ isSubmitting, handleSubmit, handleReset, values }) => (
+            <Form onSubmit={handleSubmit}>
+              <TextField placeholder="Title" name="title" />
+              <TextField placeholder="Location" name="location" />
+              <TextField
+                name="description"
+                textbox={true}
+                placeholder="Description"
+              />
+              <Box my="2em">
+                <Heading fontSize={"1em"} mb="1em">
+                  Upload Images
+                </Heading>
+                <UploadFile images={images} setImages={setImages} />
+              </Box>
+
+              <Stack direction="row" justifyContent="space-between">
+                <Button
+                  isLoading={isSubmitting}
+                  disabled={isSubmitting}
+                  loadingText="Submitting"
+                  // width={["95%", "30%"]}
+                  size="lg"
+                  type="submit"
+                >
+                  Submit
+                </Button>
+                <Button
+                  size="sm"
+                  colorScheme="red"
+                  onClick={handleReset}
+                  disabled={isSubmitting}
+                >
+                  CLEAR
+                </Button>
+              </Stack>
+
+              {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
+            </Form>
+          )}
+        </Formik>
+      </Container>
+    </PageContainer>
+  );
 };
 
 export default CreateTrail;
