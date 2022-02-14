d76c1bb7e964696c1d0dc26175841b8855b15f05
jeremiahcanlas <jeremiahcanlas@gmail.com>
2022-02-13 22:48:53 -0500

    added mandatory action for antd upload

--- components/UploadFile.js
+++ components/UploadFile.js
@@ -47,6 +47,7 @@ const UploadFile = ({ images, setImages }) => {
   return (
     <>
       <Upload
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
         listType="picture-card"
         fileList={images}
         onPreview={handlePreview}
